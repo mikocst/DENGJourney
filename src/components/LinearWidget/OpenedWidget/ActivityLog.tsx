225f41f2199ff673c5891b4b97a4203921d865b1
mikocst <mikocastillo7@gmail.com>
2026-02-02 23:50:59 -0600

    Finished base UI besides activityLog component.

--- src/components/LinearWidget/OpenedWidget/ActivityLog.tsx
+++ src/components/LinearWidget/OpenedWidget/ActivityLog.tsx
@@ -1,9 +1,19 @@
-
+import Comments from "./Comments"
 
 const ActivityLog = () => {
   return (
-    <div className = "flex flex-col gap-1">
-        Activity
+    <div className = "flex flex-col gap-1 w-full">
+        <div className = "flex flex-row justify-between items-center">
+            <h2>Activity</h2>
+            <div className = "flex flex-row gap-4 justify-center items-center">
+                <p className = "text-xs text-black/40">Unsubscribe</p>
+                <div className = "bg-gray-200 w-4 h-4 rounded-lg "></div>
+            </div>
+        </div>
+        <div>
+            <p>Activity Log Component</p>
+        </div>
+        <Comments/>
     </div>
   )
 }
---
+++ src/components/LinearWidget/OpenedWidget/Comments.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Comments = () => {
+  return (
+    <div className = "pb-2 relative">
+        <textarea className = "resize-none w-full p-2 bg-gray-100 rounded-sm border border-gray-200"
+        placeholder='Leave a comment...'
+        autoCorrect='true'
+        >
+
+        </textarea>
+    </div>
+  )
+}
+
+export default Comments
