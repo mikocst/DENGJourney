a38ec5fbde55cf7715f5b0841b8274c3456237c5
mikocst <mikocastillo7@gmail.com>
2026-02-04 23:33:27 -0600

    Base animation for widget switching content created

--- src/components/LinearWidget/Widget.tsx
+++ src/components/LinearWidget/Widget.tsx
@@ -1,19 +1,50 @@
 import { useState } from "react"
 import WidgetClosed from "./ClosedWidget/WidgetClosed";
 import WidgetOpened from "./OpenedWidget/WidgetOpened";
+import { motion, AnimatePresence, } from "motion/react";
 
 const Widget = () => {
 
-    const [isClosed, setIsClosed] = useState(false);
+    const [isClosed, setIsClosed] = useState(true);
 
     const handleIsClosed = () => {
+      
       setIsClosed(!isClosed)
     }
 
   return (
-    <div className = "bg-gray-200/20 w-sm px-2 rounded-md border border-gray-300 h-auto">
-        <WidgetOpened/>
-    </div>
+    <motion.div
+    layout
+    animate ={{height: 'auto'}}
+    transition = {{type: "spring", bounce: 0.2, stiffness: 500, damping: 30, duration: 0.3}}
+    onClick={() => handleIsClosed()}
+    className = "bg-gray-200/20 w-sm p-2 rounded-md border border-gray-300 h-auto overflow-hidden">
+        <AnimatePresence
+        mode = "popLayout"
+        >
+          {isClosed === true ? (
+            <motion.div
+            key = "closed"
+            initial = {{opacity: 0}}
+            animate = {{opacity: 1}}
+            exit = {{opacity: 0, filter: "blur(10px)", y: -20}}
+            transition = {{duration: 0.2, delay: 0.2}}
+            >
+              <WidgetClosed/>
+            </motion.div>
+          ) : (
+            <motion.div
+            key = "open"
+            initial = {{opacity: 0}}
+            animate = {{opacity: 1}}
+            exit = {{opacity: 0, filter: "blur(10px)"}}
+            transition = {{duration: 0.2, delay: 0.2}}
+            >
+              <WidgetOpened/>
+            </motion.div>
+          )}
+        </AnimatePresence>
+    </motion.div>
   )
 }
 
