05d40a47e7de48d078472d3772671f0bbb65adc9
Miko Castillo <mikocastillo7@gmail.com>
2026-02-13 17:00:39 -0600

    styled text and adjusted padding

--- src/components/LinearWidget/OpenedWidget/ActivityLog.tsx
+++ src/components/LinearWidget/OpenedWidget/ActivityLog.tsx
@@ -2,7 +2,7 @@ import Comments from "./Comments"
 
 const ActivityLog = () => {
   return (
-    <div className = "flex flex-col gap-1 w-full">
+    <div className = "flex flex-col gap-2 w-full">
         <div className = "flex flex-row justify-between items-center">
             <h2>Activity</h2>
             <div className = "flex flex-row gap-4 justify-center items-center">
@@ -10,8 +10,9 @@ const ActivityLog = () => {
                 <div className = "bg-gray-200 w-4 h-4 rounded-lg "></div>
             </div>
         </div>
-        <div>
-            <p>Activity Log Component</p>
+        <div className = "flex flex-col gap-1">
+            <p className = "text-sm text-black/50">02/13/15: Miko raised status of issue to high priority</p>
+            <p className = "text-sm text-black/50">02/04/25: Miko created the issue</p>
         </div>
         <Comments/>
     </div>
--- src/components/LinearWidget/OpenedWidget/Comments.tsx
+++ src/components/LinearWidget/OpenedWidget/Comments.tsx
@@ -3,11 +3,10 @@ import React from 'react'
 const Comments = () => {
   return (
     <div className = "pb-2 relative">
-        <textarea className = "resize-none w-full p-2 bg-gray-100/60 rounded-sm border border-gray-200"
+        <textarea className = "resize-none w-full p-2 bg-gray-100/60 rounded-sm border border-gray-200 mt-2"
         placeholder='Leave a comment...'
         autoCorrect='true'
         >
-
         </textarea>
     </div>
   )
