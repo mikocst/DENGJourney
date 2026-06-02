e770ffd2b36f1751dc03cbaa9dfd6ba12713d0a8
mikocst <mikocastillo7@gmail.com>
2026-06-01 19:37:11 -0500

    Baseline dock menu implemented

--- src/components/DockMenu/DockWrapper.tsx
+++ src/components/DockMenu/DockWrapper.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ChevronUpCircle } from 'lucide-react'
+import { ChevronUpCircle, NotebookPen, CalendarDays, KanbanSquare,  Bot } from 'lucide-react'
 import { motion, AnimatePresence } from 'motion/react'
 
 const DockWrapper = () => {
@@ -7,7 +7,7 @@ const DockWrapper = () => {
   const [isHover, setIsHover] = useState<boolean>(false);
 
   return (
-    <div className = "flex flex-col items-center gap-2 relative"
+    <div className = "flex flex-col items-center gap-2 relative p-2 w-48"
          onMouseEnter={() => setIsHover(true)}
          onMouseLeave={() => setIsHover(false)}
     >
@@ -15,7 +15,7 @@ const DockWrapper = () => {
                 {isHover && (
                     <motion.button key = "expand" 
                     initial = {{opacity: 0, y:0}}
-                    animate = {{opacity: 1, y: -30}}
+                    animate = {{opacity: 1, y: -20}}
                     exit = {{opacity: 0, y:0}}
                     transition={{ease: 'easeOut', duration: 0.2}}
                     className = "bg-gray-200/50 p-1 rounded-4xl absolute top-0">
@@ -24,9 +24,12 @@ const DockWrapper = () => {
                 )}
             </AnimatePresence>
         <div 
-        className = "p-2 bg-gray-100/70 rounded-md shadow-sm relative w-full"
+        className = "p-2 bg-gray-100/70 rounded-md shadow-sm relative w-full flex flex-row justify-between items-center"
         >
-            DockWrapper
+            <NotebookPen size={"18px"} className = "text-gray-600"/>
+            <CalendarDays size = {"18px"} className = "text-gray-600"/>
+            <KanbanSquare size = {"18px"} className = "text-gray-600"/>
+            <Bot size = {"18px"} className = "text-gray-600"/>
         </div>
     </div>
   )
